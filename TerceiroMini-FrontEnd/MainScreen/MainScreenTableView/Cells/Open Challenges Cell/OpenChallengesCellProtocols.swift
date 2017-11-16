6b2c8269cb00a3444c196ca14ef38a6edad9234e
Leonel <mml_leo@hotmail.com>
2017-11-16 19:52:45 -0200

    Project UI structure finished, challenge files structure finished

---
+++ TerceiroMini-FrontEnd/MainScreen/MainScreenTableView/Cells/Open Challenges Cell/OpenChallengesCellProtocols.swift
@@ -0,0 +1,9 @@
+//
+//  OpenChallengesCellProtocols.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Leonel Menezes on 16/11/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import Foundation

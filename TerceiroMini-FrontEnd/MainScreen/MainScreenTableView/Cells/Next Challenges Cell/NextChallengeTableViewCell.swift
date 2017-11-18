cde06547227585e2cd49feb6ebb1587e5c0e1e96
Leonel <mml_leo@hotmail.com>
2017-11-18 21:42:12 -0200

    tela quase pronta, falta organizar a arquitetura, constraints e o workflow do caroussel

---
+++ TerceiroMini-FrontEnd/MainScreen/MainScreenTableView/Cells/Next Challenges Cell/NextChallengeTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  NextChallengeTableViewCell.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Leonel Menezes on 18/11/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import UIKit
+
+class NextChallengeTableViewCell: UITableViewCell, UICollectionViewDataSource, UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "nextChallenge", for: indexPath) as! NextChallengesCollectionViewCell
+        
+        cell.dateLabel.text = "DATE"
+        cell.themeImage.image = UIImage(named: "pombo")
+        cell.themeLabel.text = "pombo"
+        
+        return cell
+    }
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

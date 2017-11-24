38eca3f303095415d6e474e5fc9efbae8b047450
Leonel <mml_leo@hotmail.com>
2017-11-24 15:52:54 -0200

    starting page view

---
+++ TerceiroMini-FrontEnd/MainScreen/NavigationViewController.swift
@@ -0,0 +1,58 @@
+//
+//  NavigationViewController.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Leonel Menezes on 24/11/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import UIKit
+
+class NavigationViewController: UIPageViewController, UIPageViewControllerDataSource {
+    
+    lazy var viewControllerList:[UIViewController] = {
+        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let vc1 = sb.instantiateViewController(withIdentifier: "MainStoryboard")
+        let vc2 = sb.instantiateViewController(withIdentifier: "ProfileStoryboard")
+        
+        return [vc1, vc2]
+    }()
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.dataSource = self
+        
+        if let firstViewController = viewControllerList.first {
+            self.setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
+        }
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

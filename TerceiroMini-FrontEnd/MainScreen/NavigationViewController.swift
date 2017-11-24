2629a6093a2377f33c671ba4c6aad84a960777a3
Leonel <mml_leo@hotmail.com>
2017-11-24 16:25:40 -0200

    added pageview

--- TerceiroMini-FrontEnd/MainScreen/NavigationViewController.swift
+++ TerceiroMini-FrontEnd/MainScreen/NavigationViewController.swift
@@ -11,7 +11,7 @@ import UIKit
 class NavigationViewController: UIPageViewController, UIPageViewControllerDataSource {
     
     lazy var viewControllerList:[UIViewController] = {
-        let sb = UIStoryboard(name: "Main", bundle: nil)
+        let sb = UIStoryboard(name: "MainScreen", bundle: nil)
         let vc1 = sb.instantiateViewController(withIdentifier: "MainStoryboard")
         let vc2 = sb.instantiateViewController(withIdentifier: "ProfileStoryboard")
         
@@ -38,11 +38,28 @@ class NavigationViewController: UIPageViewController, UIPageViewControllerDataSo
     
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        return nil
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let previousIndex = vcIndex - 1
+        
+        guard previousIndex >= 0 else {return nil}
+        
+        guard viewControllerList.count > previousIndex else {return nil}
+        
+        return viewControllerList[previousIndex]
     }
     
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        return nil
+        
+        guard let vcIndex = viewControllerList.index(of: viewController) else {return nil}
+        
+        let nextIndex = vcIndex + 1
+        
+        guard viewControllerList.count != nextIndex else {return nil}
+        
+        guard viewControllerList.count > nextIndex else {return nil}
+        
+        return viewControllerList[nextIndex]
     }
 
     /*

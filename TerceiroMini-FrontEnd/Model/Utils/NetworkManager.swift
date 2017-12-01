f85f34f324d99eb0805d142da3040bc8418ffa89
Angrear <folameloma@gmail.com>
2017-12-01 17:23:44 -0200

    NetworkManager methods made class funds

--- TerceiroMini-FrontEnd/Model/Utils/NetworkManager.swift
+++ TerceiroMini-FrontEnd/Model/Utils/NetworkManager.swift
@@ -14,59 +14,59 @@ class NetworkManager {
     
     // MARK: - User methods
     
-    func getAllUsers(completion: @escaping ([User]?, Error?) -> Void) {
+    class func getAllUsers(completion: @escaping ([User]?, Error?) -> Void) {
         UserNet.getAll(completion: completion)
     }
     
-    func addUser(_ user: User, password: String, completion: @escaping (User?, String?, Error?) -> Void) {
+    class func addUser(_ user: User, password: String, completion: @escaping (User?, String?, Error?) -> Void) {
         UserNet.add(user: user, password: password, completion: completion)
     }
     
-    func getUser(byToken token: String, completion: @escaping (User?, Error?) -> Void) {
+    class func getUser(byToken token: String, completion: @escaping (User?, Error?) -> Void) {
         UserNet.get(byToken: token, completion: completion)
     }
     
-    func getUser(byId id: String, completion: @escaping (User?, Error?) -> Void) {
+    class func getUser(byId id: String, completion: @escaping (User?, Error?) -> Void) {
         UserNet.get(byId: id, completion: completion)
     }
     
-    func createLogin(email: String, username: String, password: String, completion: @escaping (Bool) -> Void) {
+    class func createLogin(email: String, username: String, password: String, completion: @escaping (Bool) -> Void) {
         UserNet.createLogin(username: username, email: email, password: password, completion: completion)
     }
     
-    func deleteUser(byId id: String, completion: @escaping (Bool) -> Void) {
+    class func deleteUser(byId id: String, completion: @escaping (Bool) -> Void) {
         UserNet.delete(byId: id, completion: completion)
     }
     
     // MARK: - Photo methods
     
-    func getAllPhotos(completion: @escaping ([Photo]?, Error?) -> Void) {
+    class func getAllPhotos(completion: @escaping ([Photo]?, Error?) -> Void) {
         PhotoNet.getAll(completion: completion)
     }
     
-    func addPhoto(_ photo: Photo, completion: @escaping (Photo?, Error?) -> Void) {
+    class func addPhoto(_ photo: Photo, completion: @escaping (Photo?, Error?) -> Void) {
         PhotoNet.add(photo: photo, completion: completion)
     }
     
-    func getPhotos(byToken token: String, completion: @escaping ([Photo]?, Error?) -> Void) {
+    class func getPhotos(byToken token: String, completion: @escaping ([Photo]?, Error?) -> Void) {
         PhotoNet.get(byToken: token, completion: completion)
     }
     
-    func getPhotos(byChallengeId id: String, completion: @escaping ([Photo]?, Error?) -> Void) {
+    class func getPhotos(byChallengeId id: String, completion: @escaping ([Photo]?, Error?) -> Void) {
         PhotoNet.get(byChallengeId: id, completion: completion)
     }
     
-    func getPhoto(byId id: String, completion: @escaping (Photo?, Error?) -> Void) {
+    class func getPhoto(byId id: String, completion: @escaping (Photo?, Error?) -> Void) {
         PhotoNet.get(byId: id, completion: completion)
     }
     
-    func deletePhoto(byId id: String, completion: @escaping (Photo?, Error?) -> Void) {
+    class func deletePhoto(byId id: String, completion: @escaping (Photo?, Error?) -> Void) {
         PhotoNet.delete(byId: id, completion: completion)
     }
     
     // MARK: - Challenge methods
     
-    func getAllChallenges(completion: @escaping ([Challenge]?, Error?) -> Void) {
+    class func getAllChallenges(completion: @escaping ([Challenge]?, Error?) -> Void) {
         ChallengeNet.getAll(completion: completion)
     }
 

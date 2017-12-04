def3a78eb9e430be2db4f51e44d35fb1ce56bef3
Angrear <folameloma@gmail.com>
2017-12-04 17:22:28 -0200

    placeholder animations working

--- TerceiroMini-FrontEnd/Login Flow/Support/BottomLineTextField.swift
+++ TerceiroMini-FrontEnd/Login Flow/Support/BottomLineTextField.swift
@@ -17,14 +17,18 @@ class BottomLineTextField: UITextField, UITextFieldDelegate {
     static let nonFisrtResponderBorder: CGFloat = 1.0
     
     var bottomLine: CALayer!
+    var placeholderLbl: UILabel!
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         delegate = self
         
+        placeholderLbl = createSpecialPlaceholder()
         bottomLine = createBottomLine()
+        addSubview(placeholderLbl)
         layer.addSublayer(bottomLine)
+        layer.masksToBounds = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -32,14 +36,28 @@ class BottomLineTextField: UITextField, UITextFieldDelegate {
         
         delegate = self
         
+        placeholderLbl = createSpecialPlaceholder()
         bottomLine = createBottomLine()
+        addSubview(placeholderLbl)
         layer.addSublayer(bottomLine)
+        layer.masksToBounds = false
+    }
+    
+    func createSpecialPlaceholder() -> UILabel {
+        
+        let placeholder = UILabel(frame: CGRect(origin: CGPoint(), size: frame.size))
+        
+        placeholder.text = self.placeholder
+        self.placeholder = nil
+        placeholder.font = UIFont(name: "Montserrat-Bold", size: 13)
+        
+        return placeholder
     }
     
     func createBottomLine() -> CALayer {
         
-        let size = CGSize(width: frame.width, height: 1)
-        let point = CGPoint(x: 0, y: frame.maxY - BottomLineTextField.firstResponderBorder)
+        let size = CGSize(width: frame.size.width, height: 1)
+        let point = CGPoint(x: 0, y: frame.size.height - BottomLineTextField.firstResponderBorder)
         
         let layer = CALayer()
         layer.frame = CGRect(origin: point, size: size)
@@ -52,10 +70,24 @@ class BottomLineTextField: UITextField, UITextFieldDelegate {
     func textFieldDidEndEditing(_ textField: UITextField) {
         bottomLine.borderColor = BottomLineTextField.nonFirstResponderColor.cgColor
         bottomLine.borderWidth = BottomLineTextField.nonFisrtResponderBorder
+        
+        movePlaceholder(y: 0)
     }
     
     func textFieldDidBeginEditing(_ textField: UITextField) {
         bottomLine.borderColor = BottomLineTextField.firstResponderColor.cgColor
         bottomLine.borderWidth = BottomLineTextField.firstResponderBorder
+        
+        movePlaceholder(y: -25)
+    }
+    
+    func movePlaceholder(y: CGFloat) {
+        
+        if text!.isEmpty {
+            
+            UIView.animate(withDuration: 0.3) {
+                self.placeholderLbl.frame.origin = CGPoint(x: 0, y: y)
+            }
+        }
     }
 }

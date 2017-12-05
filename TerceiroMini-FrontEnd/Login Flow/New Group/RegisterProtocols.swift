4bbde048cc7d8fb375f43c14b5213cbead2bbe3e
Angrear <folameloma@gmail.com>
2017-12-05 11:59:01 -0200

    Trying to build a responder chain

---
+++ TerceiroMini-FrontEnd/Login Flow/New Group/RegisterProtocols.swift
@@ -0,0 +1,19 @@
+//
+//  RegisterProtocols.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Gabriel Reynoso on 05/12/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import Foundation
+
+protocol RegisterPresenter {
+    
+    func goBack()
+}
+
+protocol RegisterView {
+    
+    func goBack()
+}

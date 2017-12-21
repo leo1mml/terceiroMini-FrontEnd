fa471b977777f2adcc724bed702630a489fe82cf
leo1mml <mml_leo@hotmail.com>
2017-12-20 23:26:49 -0200

    editar perfil esqueleto completo

---
+++ TerceiroMini-FrontEnd/EditProfile VC/EditProfileViewController.swift
@@ -0,0 +1,80 @@
+//
+//  EditProfileViewController.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Leonel Menezes on 20/12/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import UIKit
+
+class EditProfileViewController: UIViewController {
+
+    @IBOutlet weak var nameTextField: BottomLineTextField!
+    @IBOutlet weak var userNameTextField: BottomLineTextField!
+    @IBOutlet weak var emailTextField: BottomLineTextField!
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileImageBorderView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        initialConfigurationProfileImage()
+        
+        let outTap = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(outTap)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: NSNotification) {
+        
+        if self.view.frame.origin.y == 0{
+            self.view.frame.origin.y -= 130
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: NSNotification) {
+        if self.view.frame.origin.y != 0{
+            self.view.frame.origin.y += 130
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        self.profileImageBorderView.makeCircleBorderAnimate()
+    }
+    
+    @objc func dismissKeyboard() {
+        self.nameTextField.endEditing(false)
+        self.emailTextField.endEditing(false)
+        self.userNameTextField.endEditing(false)
+        
+    }
+    
+    @IBAction func dismiss(_ sender: Any) {
+        self.navigationController?.popViewController(animated: true)
+    }
+    
+    func initialConfigurationProfileImage(){
+        self.profileImage.layer.cornerRadius = self.profileImage.layer.bounds.width/2
+        self.profileImage.image = UIImage(named: "profile-default")
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

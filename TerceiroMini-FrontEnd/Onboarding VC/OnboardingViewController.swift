f6994fac19723095407d9d28da161810da00282f
Augusto <augusto@Mac304.local>
2017-12-21 14:09:30 -0200

    adicionando pageView

---
+++ TerceiroMini-FrontEnd/Onboarding VC/OnboardingViewController.swift
@@ -0,0 +1,92 @@
+//
+//  OnboardingViewController.swift
+//  TerceiroMini-FrontEnd
+//
+//  Created by Augusto on 21/12/2017.
+//  Copyright © 2017 BEPID. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingViewController: UIPageViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dataSource = self
+        
+        if let firstViewController = orderedViewControllers.first {
+            setViewControllers([firstViewController],
+                               direction: .Forward,
+                               animated: true,
+                               completion: nil)
+        }
+    }
+    
+    private(set) lazy var orderedViewControllers: [UIViewController] = {
+        return [self.newOnboardViewController("1"),
+                self.newOnboardViewController("2"),
+                self.newOnboardViewController("3"),
+                self.newOnboardViewController("4")]
+    }()
+    
+    private func newOnboardViewController(identifier: String) -> UIViewController {
+        return UIStoryboard(name: "Onboarding", bundle: nil) .
+            instantiateViewControllerWithIdentifier("Onboarding\(identifier)")
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let previousIndex = viewControllerIndex - 1
+        
+        guard previousIndex >= 0 else {
+            return nil
+        }
+        
+        guard orderedViewControllers.count > previousIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[previousIndex]
+    }
+
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        guard let viewControllerIndex = orderedViewControllers.indexOf(viewController) else {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        let orderedViewControllersCount = orderedViewControllers.count
+        
+        guard orderedViewControllersCount != nextIndex else {
+            return nil
+        }
+        
+        guard orderedViewControllersCount > nextIndex else {
+            return nil
+        }
+        
+        return orderedViewControllers[nextIndex]
+    }
+    
+}
+
+// MARK: UIPageViewControllerDataSource
+
+extension TutorialPageViewController: UIPageViewControllerDataSource {
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerBeforeViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+    
+    func pageViewController(pageViewController: UIPageViewController,
+                            viewControllerAfterViewController viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+}

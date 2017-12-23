70b3798f4d4aadf58ada71377991b4780fac1306
Augusto <augusto@Mac304.local>
2017-12-23 11:25:15 -0200

    montando animação

--- TerceiroMini-FrontEnd/Onboarding VC/Onboarding4ViewController.swift
+++ TerceiroMini-FrontEnd/Onboarding VC/Onboarding4ViewController.swift
@@ -10,10 +10,58 @@ import UIKit
 
 class Onboarding4ViewController: UIViewController {
 
+    @IBOutlet weak var image: UIImageView!
+    
+    @IBOutlet weak var titleText: UILabel!
+    
+    @IBOutlet weak var descText: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        
+    
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.animateLeft()
+        self.animateRigth()
+    }
+    
+    func animateLeft() {
+        UIView.animate(withDuration: 0.2, delay: 0, options: .curveLinear, animations: {
+            var imageFrame = self.image.frame
+            imageFrame.origin.x -= imageFrame.size.width/2
+            
+            var titleFrame = self.titleText.frame
+            titleFrame.origin.x -= titleFrame.size.width/2
+            
+            var descFrame = self.descText.frame
+            descFrame.origin.x -= descFrame.size.width/2
+            
+            self.image.frame = imageFrame
+            self.titleText.frame = titleFrame
+            self.descText.frame = descFrame
+        }, completion: { finished in
+            print("Basket doors opened!")
+        })
+    }
+    
+    func animateRigth() {
+        UIView.animate(withDuration: 0.8, delay: 0, options: .curveLinear, animations: {
+            var imageFrame = self.image.frame
+            imageFrame.origin.x += imageFrame.size.width/2
+            
+            var titleFrame = self.titleText.frame
+            titleFrame.origin.x += titleFrame.size.width/2
+            
+            var descFrame = self.descText.frame
+            descFrame.origin.x += descFrame.size.width/2
+            
+            self.image.frame = imageFrame
+            self.titleText.frame = titleFrame
+            self.descText.frame = descFrame
+        }, completion: { finished in
+            print("Basket doors opened!")
+        })
     }
     
     @IBAction func vamosLaButton(_ sender: Any) {
